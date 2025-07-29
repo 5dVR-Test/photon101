fe7252d990cb80a4ebc607e2efb40e111e3f051b
Mohamed Badawy <mohamed.badawy@5d-vr.com>
2025-07-29 17:25:52 +0300

    sprinting

--- Assets/Scripts/Player/Movement.cs
+++ Assets/Scripts/Player/Movement.cs
@@ -2,25 +2,36 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
+    [Header("Movement Speed")]
     public float walkSpeed = 4f;
+    public float sprintSpeed = 14f;
     public float maxVelocityChange = 18f;
+    [Space]
+    public float jumpHeight = 30f;
+    
+
     private Vector2 input;
     private Rigidbody rb;
 
+    private bool sprinting;
 
-    private void Start()
+
+    void Start()
     {
          rb = GetComponent<Rigidbody>();
     }
-    private void Update()
+    void Update()
     {
         input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
         input.Normalize();
+
+        sprinting = Input.GetButton("Sprint");
+
     }
 
     private void FixedUpdate()
     {
-        rb.AddForce(CalculateMovement(walkSpeed), ForceMode.VelocityChange);
+        rb.AddForce(CalculateMovement(sprinting ? sprintSpeed : walkSpeed), ForceMode.VelocityChange);
     }
 
     Vector3 CalculateMovement(float _speed)
---
+++ Assets/Scripts/Player/PlayerSetup.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class PlayerSetup : MonoBehaviour
+{
+   
+    public Movement movement;
+
+    public GameObject camera;
+
+
+    public void IsLocalPlayer()
+    {
+        movement.enabled = true;
+        camera.SetActive(true);
+    }
+
+    
+
+}
